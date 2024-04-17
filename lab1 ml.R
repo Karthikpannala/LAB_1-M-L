15e6df0b87088e12920255cb4996ed07d13e4768
Karthikpannala <146992861+Karthikpannala@users.noreply.github.com>
2024-04-17 19:48:54 -0400

    Update lab1 ml.R

--- lab1 ml.R
+++ lab1 ml.R
@@ -4,7 +4,7 @@
 student_data <- read.csv(r"(C:\Users\karth\Downloads\studentInfo.csv)")
 
 # Preprocessing the data: convert "disability" into a factor and "final_result" into a binary variable
-student_data$is_passed <- as.factor(ifelse(student_data$final_result == "Pass", 1, 0))
+student_data$pass <- as.factor(ifelse(student_data$final_result == "Pass", 1, 0))
 student_data$disability_status <- as.factor(student_data$disability)
 
 # Make 'imd_band' into a numerical scale according to the provided categories.
@@ -20,7 +20,7 @@ train_set <- student_data[training]
 test_set <- student_data[-training]
 
 # Building a logistic regression model with glm (Generalized Linear Model) in base R
-logit_model <- glm(is_passed ~ disability_status + imd_numeric, family = binomial(link = "logit"), data = train_set)
+logit_model <- glm(pass ~ disability_status + imd_numeric, family = binomial(link = "logit"), data = train_set)
 
 # Display Model Summary
 summary(logit_model)
@@ -30,7 +30,7 @@ test_predictions <- predict(logit_model, test_set, type = "response")
 predicted_outcome <- ifelse(test_predictions > 0.5, 1, 0)
 
 # Calculating the Accuracy
-true_outcomes <- as.numeric(test_set$is_passed) - 1  # Adjusting factor levels from 1 to 0 and 1 for comparison
+true_outcomes <- as.numeric(test_set$pass) - 1  # Adjusting factor levels from 1 to 0 and 1 for comparison
 model_accuracy <- mean(predicted_outcome == true_outcomes)
 print(paste("Model Accuracy:", model_accuracy))
 
